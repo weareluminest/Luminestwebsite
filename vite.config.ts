e5124751ccf696ce6742142026e18988107cf1cb
weareluminest <contact@weareluminest.co.uk>
2025-12-03 16:59:56 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // <---- ISSO AQUI RESOLVE O ERRO DO MIME TYPE
+  base: "/", 
   resolve: {
     extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
     alias: {
